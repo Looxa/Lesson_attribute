94ddfbea091516a0fe07927638b939498fc7fa91
Looxa <imxoloxas@gmail.com>
2022-02-23 14:07:38 +0300

    Исправлена переменная

---
+++ ForClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Lesson_attribute;
+
+namespace Lesson_attribute
+{
+    [ThisAttributeForClass("SomeText")]
+    public class ForClass
+    {
+        [ThisAttributeForProp]
+        public string ForAttributeProp { get; set; }
+        [ThisAttributeForMethod]
+        public void WorkInClass(string text)
+        {
+            Console.WriteLine("Выполнена работа классом, в который было передано значение атрибута: {0}", text);
+        }
+
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]  //Аттрибут 1 - Для метода
+    public class ThisAttributeForMethod : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]   //Аттрибут 2  - Для класса
+    public class ThisAttributeForClass : Attribute
+    {
+        public ThisAttributeForClass(string text)
+        {
+            textForThisAttribute = text;
+        }
+        public string textForThisAttribute { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)] //Аттрибут 3 - Для свойства
+    public class ThisAttributeForProp : Attribute
+    {
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Lesson_attribute;
+
+namespace Lesson_AttributeTest
+{
+    public class Programm
+    {
+        private static Assembly textForAttribut;
+
+        public static void Main()
+        {
+            var attributeForClass = typeof(ForClass).GetCustomAttribute<ThisAttributeForClass>();
+            Console.WriteLine(attributeForClass);
+            var newClass = new ForClass();
+            var textFromAttribute = GetAttribute(typeof(ForClass));
+            newClass.WorkInClass(textFromAttribute);
+            Console.ReadKey();
+        }
+
+
+
+        public static string GetAttribute(Type textForAttribut)
+        {
+            // Get instance of the attribute.
+            ThisAttributeForClass MyAttribute =
+                (ThisAttributeForClass)Attribute.GetCustomAttribute(textForAttribut, typeof(ThisAttributeForClass));
+
+            if (MyAttribute == null)
+            {
+                Console.WriteLine("Атрибут не найден, либо нет значения в атрибуте");
+                return MyAttribute.textForThisAttribute = "Null";
+            }
+            else
+            {
+                return MyAttribute.textForThisAttribute;
+
+            }
+        }
+    }
+
+}
